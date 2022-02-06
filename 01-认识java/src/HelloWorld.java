220055cb8213833876a97b31eb34aaaa7ad14678
fraudLeo <fraudLeo1@Gmail.com>
2022-02-06 11:19:55 +0800

    my JAvA

---
+++ 01-认识java/src/HelloWorld.java
@@ -0,0 +1,17 @@
+//public 表示是公开的
+//class表示定义一个类
+//HelloWorld 表示一个类名
+public class HelloWorld {//表示定义一个公开的类,起名为HelloWorld
+
+    /**
+     * public 表示公开的
+     * static 表示静态的
+     * void 表示返回值是空
+     * main表示方法名是main
+     * (String[] args) 是一个main方法的形式参数列表
+     */
+
+    public static void main(String[] args) {//表示定义一个公开的 静态的 之方法------程序的入口
+        System.out.println("Hello World!");
+    }
+}
