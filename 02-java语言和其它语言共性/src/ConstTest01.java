e2f6c4b7eda62a6d25ab9a6da9a1992f128fb7ae
fraudLeo <fraudLeo1@Gmail.com>
2022-02-06 21:21:14 +0800

    my JAvA

---
+++ 02-java语言和其它语言共性/src/ConstTest01.java
@@ -0,0 +1,15 @@
+/**
+ * 关于字面值
+ *      字面值:看到这个数据就知道这个数据,
+ *          -10
+ *          -100
+ *          -"abc"
+ *          -'a'
+ *          -true
+ *          -false
+ */
+public class ConstTest01 {
+    public static void main(String[] args) {
+
+    }
+}
