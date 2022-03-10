127bf638e75904757fd02fa37a840161253d6910
fraudLeo <fraudLeo1@Gmail.com>
2022-03-10 20:31:37 +0800

    回宿舍整理代码(更新到礼物

--- 蓝桥杯/算法训练/src/无聊的逗.java
+++ 蓝桥杯/算法训练/src/无聊的逗.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 无聊的逗 {
@@ -11,7 +12,24 @@ public class 无聊的逗 {
         int N = sc.nextInt();
         int sum = 0;
         int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr,0,arr.length);
 
+        int len_choose = 0;
+        for (int i = 0; i < N; i++) {
+            sum+=arr[i];
+        }
 
+        double middle_num = sum/2.0;
+
+        for (int i = N-1; i >=0; i--) {
+            if (middle_num>=arr[i]+len_choose) {
+                len_choose+=arr[i];
+            }
+        }
+
+        System.out.println(len_choose);
     }
 }
---
+++ 蓝桥杯/算法训练/src/礼物.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 礼物 {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    private static void method1() {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int S = sc.nextInt();
+
+        int[] arrs = new int[N];
+        int[] choose = new int[999];
+        int sum1 = 0;
+        int sum2 = 0;
+        int i1;
+        int i2;
+        int m = 0;
+        for (int i = 0; i < N; i++) {
+            arrs[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i <=2/N; i++) {
+            for (int j = 0; j <=N-i; j++) {
+
+                for ( i1 = j; i1 <= j + i; i1++) {
+                    sum1 +=arrs[i1];
+                }
+                for ( i2 = i1+i;i2<=j+i +i;i2++) {
+                    sum2 +=arrs[i2];
+                }
+                if (sum2==sum1&&sum1<=S) {
+                    choose[m] = 2*i;
+                    m++;
+                }
+
+            }
+        }
+
+
+        int end = Arrays.stream(choose).max().getAsInt();
+        System.out.println(end);
+    }
+
+}
